3b88a758ee3d5c86652935410ad91d872c63212a
Jhonnyfm <fonseca.dionisio@gmail.com>
2017-03-08 23:16:52 -0500

    Add files via upload

---
+++ culqi/src/Cliente.php
@@ -0,0 +1,9 @@
+<?php
+namespace DigitalWeb\Culqi;
+
+
+class Cliente {
+	public static function Testing() {
+		return "Hello World";
+	}
+}
---
+++ culqi/src/Token.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DigitalWeb\Culqi;
+
+
+class Token {
+	public static function unMetodoEstatico() {
+		echo "Mi Metodo";
+	}
+}
